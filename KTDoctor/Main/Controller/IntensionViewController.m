6928c48c40b51f4c5767ba2e32689a5fe9174c7a
dezhi <gary.chen.kel@gmail.com>
2019-04-30 17:43:04 +0800

    强度测试授权

--- KTDoctor/Main/Controller/IntensionViewController.m
+++ KTDoctor/Main/Controller/IntensionViewController.m
@@ -8,72 +8,300 @@
 
 #import "IntensionViewController.h"
 #import <AVFoundation/AVFoundation.h>
+#import "SGQRCode.h"
+#import "MainViewController.h"
+#import "UserModel.h"
 
-@interface IntensionViewController ()<AVCaptureMetadataOutputObjectsDelegate>
-@property (nonatomic, strong) AVCaptureSession *session;
+#define kBackButton_LeftMargin 15
+#define kButton_Height 30
+
+@interface IntensionViewController ()<AVCaptureMetadataOutputObjectsDelegate>{
+    SGQRCodeObtain *obtain;
+}
+
+@property(nonatomic,strong)SGQRCodeScanView *scanView;
+@property(nonatomic,strong)UIButton *flashlightBtn;
+@property(nonatomic,strong)UILabel *promptLabel;
+@property(nonatomic,assign)BOOL isSelectedFlashlightBtn;
+@property(nonatomic,strong)UserModel *user;
+@property(nonatomic,strong)UIView *navView;
+@property(nonatomic,strong)UIButton *backButton;
 @end
 
 @implementation IntensionViewController
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
-        if (granted) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                [self loadScanView];
-            });
+    // Do any additional setup after loading the view from its nib.
+    self.view.backgroundColor = [UIColor clearColor];
+    self.navigationController.navigationBar.hidden = YES;
+    [self configScanView];
+    self.user = [[UserModel sharedUserModel] getCurrentUser];
+    obtain = [SGQRCodeObtain QRCodeObtain];
+    [self setupQRCodeScan];
+    [self.view addSubview:self.scanView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    /// 二维码开启方法
+    [obtain startRunningWithBefore:nil completion:nil];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.scanView addTimer];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self.scanView removeTimer];
+    [self removeFlashlightBtn];
+    [obtain stopRunning];
+}
+
+- (void)dealloc {
+    NSLog(@"WCQRCodeVC - dealloc");
+    [self removeScanningView];
+}
+
+- (void)setupQRCodeScan {
+    __weak typeof(self) weakSelf = self;
+    
+    SGQRCodeObtainConfigure *configure = [SGQRCodeObtainConfigure QRCodeObtainConfigure];
+    configure.sampleBufferDelegate = YES;
+    [obtain establishQRCodeObtainScanWithController:self configure:configure];
+    [obtain setBlockWithQRCodeObtainScanResult:^(SGQRCodeObtain *obtain, NSString *result) {
+        if (result) {
+            [obtain stopRunning];
+            NSLog(@"result is :%@",result);
+            NSMutableDictionary *parameter = [NSMutableDictionary dictionary];
+            NSDictionary *dict = weakSelf.user.organ;
+            NSArray *orgCodeArr = [dict valueForKey:@"orgCode"];
+            NSString *orgCode = orgCodeArr[0];
+            [parameter setValue:orgCode forKey:@"orgCode"];
+            [parameter setValue:result forKey:@"qrContent"];
+            [weakSelf doctorAuth:parameter];
+        }
+    }];
+    [obtain setBlockWithQRCodeObtainScanBrightness:^(SGQRCodeObtain *obtain, CGFloat brightness) {
+        if (brightness < - 1) {
+            [weakSelf.view addSubview:weakSelf.flashlightBtn];
         } else {
-            NSLog(@"无权限访问相机");
+            if (weakSelf.isSelectedFlashlightBtn == NO) {
+                [weakSelf removeFlashlightBtn];
+            }
         }
     }];
 }
 
-- (void)loadScanView {
-    //获取摄像设备
-    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-    //创建输入流
-    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:nil];
-    //创建输出流
-    AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc]init];
-    //设置代理 在主线程里刷新
-    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    
-    //初始化链接对象
-    self.session = [[AVCaptureSession alloc]init];
-    //高质量采集率
-    [self.session setSessionPreset:AVCaptureSessionPresetHigh];
-    
-    [self.session addInput:input];
-    [self.session addOutput:output];
-    
-    //设置扫码支持的编码格式(如下设置条形码和二维码兼容)
-    output.metadataObjectTypes=@[AVMetadataObjectTypeQRCode,//二维码
-                                 //以下为条形码，如果项目只需要扫描二维码，下面都不要写
-                                 AVMetadataObjectTypeEAN13Code,
-                                 AVMetadataObjectTypeEAN8Code,
-                                 AVMetadataObjectTypeUPCECode,
-                                 AVMetadataObjectTypeCode39Code,
-                                 AVMetadataObjectTypeCode39Mod43Code,
-                                 AVMetadataObjectTypeCode93Code,
-                                 AVMetadataObjectTypeCode128Code,
-                                 AVMetadataObjectTypePDF417Code];
-    
-    AVCaptureVideoPreviewLayer *layer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
-    layer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-    layer.frame = self.view.layer.bounds;
-    [self.view.layer insertSublayer:layer atIndex:0];
-    //开始捕获
-    [self.session startRunning];
+- (SGQRCodeScanView *)scanView {
+    if (!_scanView) {
+        _scanView = [[SGQRCodeScanView alloc] initWithFrame:CGRectMake(0, 0, kWidth, kHeight)];
+        self.navView = [[UIView alloc] initWithFrame:CGRectMake(0, kNavView_TopMargin, kWidth, kNavView_Height)];
+        self.navView.backgroundColor = [UIColor clearColor];
+        [_scanView addSubview:self.navView];
+        _scanView.userInteractionEnabled = YES;
+        self.backButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.backButton setImage:[UIImage imageNamed:@"back"] forState:UIControlStateNormal];
+        self.backButton.adjustsImageWhenHighlighted = NO;
+        CGFloat backButtonTop = (kNavView_Height - kButton_Height)/2;
+        self.backButton.frame = CGRectMake(kBackButton_LeftMargin, backButtonTop, kButton_Height, kButton_Height);
+        self.backButton.layer.cornerRadius = kButton_Height / 2.0;
+        self.backButton.layer.masksToBounds = YES;
+        [self.backButton addTarget:self action:@selector(back:) forControlEvents:UIControlEventTouchUpInside];
+        [_scanView addSubview:self.backButton];
+    }
+    return _scanView;
+}
+- (void)removeScanningView {
+    [self.scanView removeTimer];
+    [self.scanView removeFromSuperview];
+    self.scanView = nil;
 }
 
-#pragma mark - AVCaptureMetadataOutputObjectsDelegate
--(void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection{
-    if (metadataObjects.count>0) {
-        [self.session stopRunning];
-        AVMetadataMachineReadableCodeObject *metadataObject = metadataObjects[0];
-        
-        NSLog(@"%@",metadataObject.stringValue);
+- (UILabel *)promptLabel {
+    if (!_promptLabel) {
+        _promptLabel = [[UILabel alloc] init];
+        _promptLabel.backgroundColor = [UIColor clearColor];
+        CGFloat promptLabelX = 0;
+        CGFloat promptLabelY = 0.73 * self.view.frame.size.height;
+        CGFloat promptLabelW = self.view.frame.size.width;
+        CGFloat promptLabelH = 25;
+        _promptLabel.frame = CGRectMake(promptLabelX, promptLabelY, promptLabelW, promptLabelH);
+        _promptLabel.textAlignment = NSTextAlignmentCenter;
+        _promptLabel.font = [UIFont boldSystemFontOfSize:13.0];
+        _promptLabel.textColor = [[UIColor whiteColor] colorWithAlphaComponent:0.6];
+        _promptLabel.text = @"将二维码/条码放入框内, 即可自动扫描";
     }
+    return _promptLabel;
+}
+
+#pragma mark - button click events
+
+- (void)back:(UIButton*)sender {
+    [self.navigationController popViewControllerAnimated:NO];
+}
+
+#pragma mark - - - 闪光灯按钮
+- (UIButton *)flashlightBtn {
+    if (!_flashlightBtn) {
+        // 添加闪光灯按钮
+        _flashlightBtn = [UIButton buttonWithType:(UIButtonTypeCustom)];
+        CGFloat flashlightBtnW = 30;
+        CGFloat flashlightBtnH = 30;
+        CGFloat flashlightBtnX = 0.5 * (self.view.frame.size.width - flashlightBtnW);
+        CGFloat flashlightBtnY = 0.55 * self.view.frame.size.height;
+        _flashlightBtn.frame = CGRectMake(flashlightBtnX, flashlightBtnY, flashlightBtnW, flashlightBtnH);
+        [_flashlightBtn setBackgroundImage:[UIImage imageNamed:@"SGQRCodeFlashlightOpenImage"] forState:(UIControlStateNormal)];
+        [_flashlightBtn setBackgroundImage:[UIImage imageNamed:@"SGQRCodeFlashlightCloseImage"] forState:(UIControlStateSelected)];
+        [_flashlightBtn addTarget:self action:@selector(flashlightBtn_action:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _flashlightBtn;
+}
+
+- (void)flashlightBtn_action:(UIButton *)button {
+    if (button.selected == NO) {
+        [obtain openFlashlight];
+        self.isSelectedFlashlightBtn = YES;
+        button.selected = YES;
+    } else {
+        [self removeFlashlightBtn];
+    }
+}
+
+- (void)removeFlashlightBtn {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [obtain closeFlashlight];
+        self.isSelectedFlashlightBtn = NO;
+        self.flashlightBtn.selected = NO;
+        [self.flashlightBtn removeFromSuperview];
+    });
+}
+
+- (void)doctorAuth:(NSMutableDictionary *)parameter {
+    __weak typeof (self)weakSelf = self;
+    [[NetworkService sharedInstance] requestWithUrl:[NSString stringWithFormat:@"%@%@",kSERVER_URL,kQR_DOCTOR_AUTH_URL] andParams:parameter andSucceed:^(NSDictionary *responseObject) {
+        NSInteger code = [[responseObject valueForKey:@"code"] longValue];
+        NSString *msg = [responseObject valueForKey:@"msg"];
+        if (code == 0) {
+            [STTextHudTool showText:@"授权成功"];
+        } else if (code == 10011) {
+            [STTextHudTool showText:@"该账号已在其他设备登录或已过期"];
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"ClearLonginInfoNotification" object:nil];
+            [weakSelf.navigationController popToRootViewControllerAnimated:NO];
+        } else {
+            [STTextHudTool showText:msg];
+        }
+        NSArray *tempCVs = [weakSelf.navigationController viewControllers];
+        for (UIViewController *vc in tempCVs) {
+            if ([vc isKindOfClass:[MainViewController class]]) {
+                [weakSelf.navigationController popToViewController:vc animated:NO];
+            }
+        }
+        [weakSelf.navigationController popToRootViewControllerAnimated:NO];
+    } andFaild:^(NSError *error) {
+        NSLog(@"error :%@",error);
+    }];
+}
+
+- (void)configScanView {
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        switch (status) {
+            case AVAuthorizationStatusNotDetermined: {
+                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                    if (granted) {
+                        NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
+                    } else {
+                        NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
+                    }
+                }];
+                break;
+            }
+            case AVAuthorizationStatusAuthorized: {
+                break;
+            }
+            case AVAuthorizationStatusDenied: {
+                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+                UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                    
+                }];
+                
+                [alertC addAction:alertA];
+                [self presentViewController:alertC animated:YES completion:nil];
+                break;
+            }
+            case AVAuthorizationStatusRestricted: {
+                NSLog(@"因为系统原因, 无法访问相册");
+                break;
+            }
+                
+            default:
+                break;
+        }
+        return;
+    }
+    
+    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    [alertC addAction:alertA];
+    [self presentViewController:alertC animated:YES completion:nil];
+}
+
+- (void)QRCodeScanVC:(UIViewController *)scanVC {
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device) {
+        AVAuthorizationStatus status = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        switch (status) {
+            case AVAuthorizationStatusNotDetermined: {
+                [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL granted) {
+                    if (granted) {
+                        dispatch_sync(dispatch_get_main_queue(), ^{
+                            [self.navigationController pushViewController:scanVC animated:YES];
+                        });
+                        NSLog(@"用户第一次同意了访问相机权限 - - %@", [NSThread currentThread]);
+                    } else {
+                        NSLog(@"用户第一次拒绝了访问相机权限 - - %@", [NSThread currentThread]);
+                    }
+                }];
+                break;
+            }
+            case AVAuthorizationStatusAuthorized: {
+                [self.navigationController pushViewController:scanVC animated:YES];
+                break;
+            }
+            case AVAuthorizationStatusDenied: {
+                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+                UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+                    
+                }];
+                
+                [alertC addAction:alertA];
+                [self presentViewController:alertC animated:YES completion:nil];
+                break;
+            }
+            case AVAuthorizationStatusRestricted: {
+                NSLog(@"因为系统原因, 无法访问相册");
+                break;
+            }
+                
+            default:
+                break;
+        }
+        return;
+    }
+    
+    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"未检测到您的摄像头" preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    
+    [alertC addAction:alertA];
+    [self presentViewController:alertC animated:YES completion:nil];
 }
 
 @end
